436ee8db0c91229322437420eb837f1fe6d84acd
Tahir Əsədov <tahir-asadov@outlook.com>
2019-08-10 16:45:13 +0400

    Miqrasiyalar əlavə edildi

---
+++ travel/database/migrations/2019_08_10_121636_create_flights_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('company');
+            $table->string('plane');
+            $table->dateTime('arrival');
+            $table->decimal('price', 5, 2);
+            $table->string('to');
+            $table->string('from');
+            $table->integer('passenger');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
---
+++ travel/database/migrations/2019_08_10_121704_create_hotels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('star');
+            $table->string('city');
+            $table->string('address');
+            $table->decimal('price', 5, 2);
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+}
